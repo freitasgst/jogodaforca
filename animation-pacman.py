7b697e8e5feab5ca84b872367e31aab1028f8e67
CibeleGameleira <cibelewbcmendes@gmail.com>
2024-06-02 20:24:33 -0300

    feat(feature/tratativa-erros): Inclui esboco para pacman feliz e comendo

--- animation-pacman.py
+++ animation-pacman.py
@@ -1,18 +1,66 @@
-animation1PacMan = r' __________  '
-animation2PacMan = r'/    X  X  \ '
-animation3PacMan = r'|          | '
-animation4PacMan = r'|     /\/\/  '
-animation5PacMan = r'|    /       '
-animation6PacMan = r'|    \       '
-animation7PacMan = r'|     \/\/\  '
-animation8PacMan = r'|          | '
-animation9PacMan = r'\__________/ '
+animation1Pacman = r' __________  '
+animation2Pacman = r'/    X  X  \ '
+animation3Pacman = r'|          | '
+animation4Pacman = r'|     /\/\/  '
+animation5Pacman = r'|    /       '
+animation6Pacman = r'|    \       '
+animation7Pacman = r'|     \/\/\  '
+animation8Pacman = r'|          | '
+animation9Pacman = r'\__________/ '
+animation10Pacman = r'|      ___ |'
+animation11Pacman = r'|     /___\|'
+animation12Pacman = r'|     \___/|'
+animation13Pacman = r'|          |'
+animation14Pacman = r'/   X  X   \ '
+animation16Pacman = r'|    /  __   '
+animation17Pacman = r'|    \ |__|  '
+animationFood1 = r' __'
+animationFood2 = r'|__|'
 
-matriz = [[animation1PacMan],[animation2PacMan], [animation3PacMan], [animation4PacMan], [animation5PacMan], [animation6PacMan], [animation7PacMan], [animation8PacMan], [animation9PacMan]]
+
+def showGoodPacman():
+    goodPacman = [[animation1Pacman],
+                  [animation2Pacman],
+                [animation3Pacman],
+                [animation10Pacman],
+                [animation11Pacman],
+                [animation12Pacman],
+                [animation13Pacman],
+                [animation8Pacman],
+                [animation9Pacman]]
+    
+    for i in range(len(goodPacman)):
+        print(goodPacman[i][0])
+
 
 def showHungryPacman():
-    for i in range(len(matriz)):
-        print(matriz[i][0])
+    hungryPackman = [[animation1Pacman], 
+          [animation2Pacman], 
+          [animation3Pacman], 
+          [animation4Pacman], 
+          [animation5Pacman], 
+          [animation6Pacman], 
+          [animation7Pacman], 
+          [animation8Pacman], 
+          [animation9Pacman]]
+    
+    for i in range(len(hungryPackman)):
+        print(hungryPackman[i][0])
 
+def showEatingPacman():
+    hungryPackman = [[animation1Pacman], 
+          [animation2Pacman], 
+          [animation3Pacman], 
+          [animation4Pacman], 
+          [animation16Pacman], 
+          [animation17Pacman], 
+          [animation7Pacman], 
+          [animation8Pacman], 
+          [animation9Pacman]]
+      
+    for i in range(len(hungryPackman)):
+        print(hungryPackman[i][0])
+
+showGoodPacman()
 showHungryPacman()
- 
+showEatingPacman()
