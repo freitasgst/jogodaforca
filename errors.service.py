6339e88ebb092033f13218477d7c215fa54f4cc1
CibeleGameleira <cibelewbcmendes@gmail.com>
2024-06-09 17:35:07 -0300

    feat(feature/tratativa-erros): Mergea com a branch testesFuncoes

--- errors.service.py
+++ errors.service.py
@@ -1,11 +1,11 @@
 import animationPacman
 
 def showResult(quantityAvaliable, quantityErrors): 
-    if(quantityAvaliable > 0):
+    if(quantityAvaliable > 0 and quantityErrors > 0):
         animationPacman.showError(quantityAvaliable, quantityErrors)
+    elif(quantityAvaliable == 7 and quantityErrors == 0):
+        animationPacman.showPositonPacman(quantityAvaliable, quantityErrors)
     elif(quantityAvaliable == 0):
-        animationPacman.showGameOver();
+        animationPacman.showGameOver()
     else:
         animationPacman.showRight(quantityAvaliable, quantityErrors)
-
-showResult(1,6)
